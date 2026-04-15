a37668824a4f8a31dc3bac1ca61fa1f6fa8706e0
JAGicana-43 <jagicana@spamast.edu.ph>
2026-04-15 20:01:35 +0800

    Step 4: assign falsy empty string

--- BuildLogicChecker_GICANA.js
+++ BuildLogicChecker_GICANA.js
@@ -3,3 +3,7 @@ console.log(Boolean(truthyOrFalsy));
 
 truthyOrFalsy = "freeCodeCamp";
 console.log(Boolean(truthyOrFalsy));
+
+// STEP 4: assign falsy empty string
+truthyOrFalsy = "";
+console.log(Boolean(truthyOrFalsy));
