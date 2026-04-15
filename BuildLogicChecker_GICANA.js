d657d52b8e6226fdba1a5be252b6646a64def76f
JAGicana-43 <jagicana@spamast.edu.ph>
2026-04-15 20:03:04 +0800

    Step 5: remove truthy/falsy test code

--- BuildLogicChecker_GICANA.js
+++
@@ -1,9 +0,0 @@
-let truthyOrFalsy = true;
-console.log(Boolean(truthyOrFalsy));
-
-truthyOrFalsy = "freeCodeCamp";
-console.log(Boolean(truthyOrFalsy));
-
-// STEP 4: assign falsy empty string
-truthyOrFalsy = "";
-console.log(Boolean(truthyOrFalsy));
